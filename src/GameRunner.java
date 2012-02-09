66111c37a7c8db109fe36d994cf08f200f9b0324
Ryan Atallah <mail@ryanatallah.com>
2012-02-08 18:27:56 -0700

    Pretty code :)
    Applied 2-space soft tabs, more whitespace, and spaces around operators.

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,27 +1,28 @@
 
 public class GameRunner {
-    public static void main(String[] args){
-        int[][] test={
-            {0,0,8,0,0,9,0,0,3},
-            {0,6,0,3,2,8,0,5,0},
-            {9,3,0,0,0,4,0,6,1},
-            {0,0,1,0,0,5,0,0,8},
-            {5,2,0,0,0,0,0,7,4},
-            {3,0,0,9,0,0,2,0,0},
-            {7,4,0,1,0,0,0,8,6},
-            {0,9,0,5,7,3,0,4,0},
-            {2,0,0,4,0,0,5,0,0}
-        };
+  public static void main(String[] args){
 
-        System.out.println("INITIAL BOARD");
-        SudokuGame2.printBoard(test);
-        System.out.println("\n\n");
+    int[][] test = {
+      {0,0,8,0,0,9,0,0,3},
+      {0,6,0,3,2,8,0,5,0},
+      {9,3,0,0,0,4,0,6,1},
+      {0,0,1,0,0,5,0,0,8},
+      {5,2,0,0,0,0,0,7,4},
+      {3,0,0,9,0,0,2,0,0},
+      {7,4,0,1,0,0,0,8,6},
+      {0,9,0,5,7,3,0,4,0},
+      {2,0,0,4,0,0,5,0,0}
+    };
 
-        SudokuGame2 trial1=new SudokuGame2(test);
-        trial1.makePossArr();
-        trial1.solveArr();
-        int[][] solvedPuzzle=trial1.getBoard();
-        System.out.println("SOLVED BOARD");
-        SudokuGame2.printBoard(solvedPuzzle);
-    }
+    System.out.println("INITIAL BOARD");
+    SudokuGame.printBoard(test);
+    System.out.println("\n\n");
+
+    SudokuGame trial1 = new SudokuGame(test);
+    trial1.makePossArr();
+    trial1.solveArr();
+    int[][] solvedPuzzle = trial1.getBoard();
+    System.out.println("SOLVED BOARD");
+    SudokuGame.printBoard(solvedPuzzle);
+  }
 }
