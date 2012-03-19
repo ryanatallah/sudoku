0845f1202f64da6f0ea8c84ea33c9d2de9496448
Emily Randall <emilymrandall@comcast.net>
2012-03-18 21:38:49 -0600

    Updated graphics

--- src/Board.java
+++ src/Board.java
@@ -1,64 +1,125 @@
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.*;
 
-public class Board extends JPanel implements ActionListener{
-  int[][] input;
-  JButton[][] buttons = new JButton[9][9];
-  
-  public Board(int[][] in){
-	input = in;
-	setBackground(Color.WHITE);
+public class Board extends JPanel implements ActionListener, MouseListener {
+  JButton[] nums;
+  TextField text = new TextField(20);
+  int padx, pady, boxSize;
+  IncompleteBoard flame = new IncompleteBoard(4);
+  int[][] board = flame.makePuzzle();
+  int[][] unsolved = new int[9][9];
+	
+  public Board(){
+    setBackground(Color.WHITE);
+    for (int a=0; a<9; a++)
+  	  for (int b=0; b<9; b++)
+  	    unsolved[a][b] = board[a][b];
   }
 
+  public void addComponentsToPane(Container c){
+	c.setLayout(null);
+	c.setBackground(Color.WHITE);
+
+	nums = new JButton[9];
+	for (int a = 1; a<10; a++){
+	  nums[a-1] = new JButton("" + a);
+	  nums[a-1].setBounds((a-1)*60+25, 500, 60, 60);
+	  nums[a-1].setActionCommand("" + a);
+	  nums[a-1].addActionListener(this);
+	  c.add(nums[a-1]);
+	}
+
+	c.addMouseListener(this);
+  }
+  
+  
   public void paintComponent(Graphics g){
 	int width = getWidth();
-	int boxSize = width/10 + 1; 
+	boxSize = 50;
+	padx = (width - boxSize*9)/2;
+	pady = 40;
 	
-	  
 	super.paintComponent(g);
 	g.setColor(Color.BLACK);
-	g.drawString("Sudoku Board", 10, 10);
+	g.setFont(new Font("Title", Font.BOLD, 30));
+	g.drawString("Sudoku Board", 200, 30);
 	
 	for (int i=0; i<=9; i++){
-	  g.drawLine(10, i*boxSize+30, width-30, i*boxSize+30);
+	  g.drawLine(padx, i*boxSize + pady, padx + boxSize*9, i*boxSize + pady);
+	  if (i%3 == 0)  
+		g.drawLine(padx, i*boxSize + pady + 1, padx + boxSize*9, i*boxSize + pady + 1);
 	}
-	g.drawLine(9, 30, 9, 300);
+
 	for (int j=0; j<=9; j++){
-	  g.drawLine(j*boxSize+10, 30, j*boxSize+10, width-10);
+	  g.drawLine(j*boxSize + padx, pady, j*boxSize + padx, pady + boxSize*9);
+	  if (j%3 == 0)
+		g.drawLine(j*boxSize + padx + 1, pady, j*boxSize + padx + 1, pady + boxSize*9);
 	}
-  }
+	
+	g.setFont(new Font("Numbers", Font.PLAIN, 30));
+	int xPos = 0;
+	int yPos = 0;
+	for (int i = 0; i < 9; i++){
+	  for (int j = 0; j < 9; j++){
+		xPos = i*boxSize + padx + boxSize/2 - 8;
+		yPos = j*boxSize + pady + boxSize/2 + 10;
+		if (board[i][j] != 0)
+		  g.drawString("" + board[i][j], xPos, yPos);
+	  }
+	}
 
-
-  public void actionPerformed(ActionEvent e){
-	System.out.println("Button clicked");
   }
   
-  public void makeButtons(){
-	  Container c = new Container();
-	  c.setLayout(new GridLayout(9, 9));
-	  c.setVisible(true);
-	  for (int a=0; a<9; a++){
-		  for (int b=0; b<9; b++){
-			String str = "" + input[a][b];
-			buttons[a][b] = new JButton(str);
-			buttons[a][b].setActionCommand("disable");
-			buttons[a][b].setToolTipText("1 2 3 4 5 6 7 8 9");
-			buttons[a][b].addActionListener(this);
-	        c.add(buttons[a][b]);
-		  }
-		}
+  public void mouseClicked(MouseEvent e) {
+    int x = e.getX();
+    int y = e.getY();
+    int i = (x - padx)/boxSize;
+    int j = (y-pady)/boxSize;
+    String str = text.getText();
+    if (str != "0" && unsolved[i][j] == 0){
+      board[i][j] = Integer.parseInt(str);
+      repaint();
+    }
+    System.out.println("X = " + i +", Y = " + j + ", text = " + str);
+  }
+
+  public void mouseEntered(MouseEvent e) {	
+  }
+
+  public void mouseExited(MouseEvent e) {
+  }
+
+  public void mousePressed(MouseEvent e) {
+  }
+
+  public void mouseReleased(MouseEvent e) {
+  }
+
+  public void actionPerformed(ActionEvent e){
+	String str = e.getActionCommand();
+	text.setText(str);
   }
   
   public static void main(String[] args){
-	JFrame w = new JFrame("Sudoku");
-	w.setBounds(300, 300, 600, 600);
-	w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	int[][] test = new CompleteBoard().makeBoard();
-	Board hope = new Board(test);
-	hope.makeButtons();
-	w.add(hope);
-	w.setResizable(false);
-	w.setVisible(true);
+	//Create and set up the window.
+      JFrame frame = new JFrame("Sudoku");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setSize(600, 600);
+      frame.setResizable(false);
+      frame.setBackground(Color.WHITE);
+      
+      Container c = frame.getLayeredPane();
+      
+      Board board = new Board();
+
+      frame.add(board);
+      board.addComponentsToPane(c);
+
+      //Display the window.
+      frame.setVisible(true);
   }
 }
+
+
--- src/IncompleteBoard.java
+++ src/IncompleteBoard.java
@@ -1,12 +1,28 @@
 import java.util.*;
 public class IncompleteBoard extends CompleteBoard{
-  int[][] puzzle;
-  int[][] solution;
-  int[][] waves;
+  int[][] puzzle = new int[9][9];
+  int[][] solution = new int[9][9];
+  int[][] waves = new int[9][9];
   Random generator = new Random();
+  int num;
+  
+  public IncompleteBoard(int diff){
+	super();
+	if (diff == 1)
+	  num = 29;
+	else if (diff == 2)
+	  num = 32;
+	else if (diff == 3)
+	  num = 35;
+	else if (diff == 4)
+	  num = 38;
+	else
+	  num = 41;
+  }
   
   public IncompleteBoard(){
 	super();
+	num = 35;
   }
   
   /**
@@ -18,20 +34,36 @@ public class IncompleteBoard extends CompleteBoard{
    */
   public int[][] makePuzzle(){
 	puzzle = super.makeBoard();
-	solution = puzzle;
-	for (int n=0; n<25; n++){
+	for (int a=0; a<9; a++){
+	  for (int b=0; b<9; b++){
+		solution[a][b] = puzzle[a][b];
+	  }
+	}
+	for (int n=0; n<num; n++){
 	  int i = generator.nextInt(9);
 	  int j = generator.nextInt(9);
 	  puzzle[i][j]=0;
 	  puzzle[8-i][8-j]=0;
 	}
-	waves = puzzle;
+	
+	for (int i=0; i<9; i++){
+	  for (int j=0; j<9; j++){
+		waves[i][j] = puzzle[i][j];
+	  }
+	}
+	
 	SudokuGame test = new SudokuGame(puzzle, true);
 	printBoard(waves, "Temporary", 0);
+
 	test.solvePuzzle();
-	printBoard(waves, "Temporary", 0);
+	
+	
 	if (test.isSolved() == true){
-	  puzzle = waves;
+	  for (int c=0; c<9; c++){
+		for (int d=0; d<9; d++){
+	      puzzle[c][d]=waves[c][d];
+		}
+	  }
 	  System.out.println("Solvable");
 	}
 	else{
